b87baf94d7b55f4b0dfad0876060232878bd8fa2
daichi0812 <ijrtrksx16.vtbvd@4rqzh@gmail.com>
2023-11-07 11:12:21 +0900

    まだ6:44

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css'
 
 function App() {
@@ -8,9 +8,13 @@ function App() {
     setCount(count + 1);
   };
 
+  useEffect(() => {
+    console.log("Hello Hooks");
+  }, []);
+
   return (
   <div className="App">
-    <h1>UseState</h1>
+    <h1>UseState, UseEffect</h1>
     <button onClick={handleClick}>＋</button>
     <p>
       {count}
