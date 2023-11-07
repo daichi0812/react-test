ba95f039c64109cc42af5679fdff9d6cbc2f21f7
daichi0812 <levi8164@icloud.com>
2023-11-07 21:58:18 +0900

    とりあえず動画一周した

--- react-hooks-tutorial-yt/src/App.jsx
+++ react-hooks-tutorial-yt/src/App.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState, useContext, useRef, useReducer, useMemo } from "react";
+import { useEffect, useState, useContext, useRef, useReducer, useMemo, useCallback } from "react";
 import './App.css'
 import ShinCodeContext from "./main";
+import SomeChild from "./SomeChild";
+import useLocalStorage from "./useLocalStorage";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -23,17 +25,44 @@ function App() {
     setCount(count + 1);
   };
 
-  //useMemo ブラウザのキャッシュに保存するメモ化
+  //useMemo メモリに保存するメモ化
   const [count01, setCount01] = useState(0);
   const [count02, setCount02] = useState(0);
 
-  const square= () => {
+  /* 
+  //  重い処理
+  const square = useMemo() => {
     let i = 0;
-    while(i < 2){
+    while(i < 200000){
       i++
     }
-    return count01 * count02;
+    return count02 * count02;
   };
+  */
+
+  const square = useMemo(() => {
+    let i = 0;
+    while(i < 2000){
+      i++
+    }
+    return count02 * count02;
+  },[count02]);
+
+  //useCallBack 関数のメモ化
+  const [counter, setCounter] = useState(0);
+
+  /*
+  const showCount = () => {
+    alert( 'これは重い処理です。' )
+  }
+  */
+
+  const showCount = useCallback(() => {
+    alert( 'これは重い処理です。' )
+    }, [counter]);
+
+    //カスタムフック
+    const [age, setAge] = useLocalStorage("age", 20);
 
   useEffect(() => {
     console.log("Hello Hooks");
@@ -72,9 +101,18 @@ function App() {
       <h1>useMemo</h1>
       <div>カウント1 : {count01}</div>
       <div>カウント2 : {count02}</div>
-      <div>結果: {square()}</div>
+      <div>結果: {square}</div>
       <button onClick={() => setCount01(count01 + 1)}>＋</button>
-      <button onClick={() => setCount02(count02 + 1)}>ー</button>
+      <button onClick={() => setCount02(count02 + 1)}>＋</button>
+
+      <hr />
+      <h1>useCallBack</h1>
+      <SomeChild showCount={showCount} />
+
+      <hr />
+      <h1>カスタムフック</h1>
+      <p>{age}</p>
+      <button onClick={() => setAge(80)}>年齢をセット</button>
     </div>
   )
 }
